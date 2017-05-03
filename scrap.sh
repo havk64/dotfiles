7a0971a0c11e5351aa4e16e7e858c8ca41ac73ad
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-05-03 15:42:31 -0300

    Uncomment variables, add while loop to fetch each address, etc

--- scrap.sh
+++ scrap.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/env bash
 # Finds the repo with more stars
 
-USER=
-REPO=
 token=9f5400d5762cdca712e84b8c921cfa801c3dbeb6
+user_agent='Holberton School' # or havk64
 header="Authorization: token $token"
 accept='Accept: application/vnd.github.v3+json'
-addr=https://api.github.com
-# dotfiles='http://dotfiles.github.io/'
+dotfiles='http://dotfiles.github.io/'
+tmpfile=github_parsed
 
-# curl "$dotfiles" | pup 'a[href] attr{href}' | grep -r 'https?://github.com' > resources
-sed -nr '/https:\/\/github.com\/[[:alpha:]]+\/[[:alpha:]]+$/p' github_users # 45 items of 91
-sed -r 's/.*stargazers_count":([[:digit"]]+).*/\1/'
+# Extract valid github addresses:
+curl "$dotfiles" | pup 'a[href] attr{href}' | grep -r 'https?://github.com' |
+# Match only valid github repositories:
+sed -nr '/https?:\/\/github.com\/[[:alnum:].-]+\/[[:alnum:].-]+\/?$/p' |
+# Fix schema url(https):
+sed -e '/http\b/s/http/https/g' > "$tmpfile"
 
-curl -H "$header" -H "$accept" "$addr/repos/""$USER"/"$REPO"
+sed -r 's/.*stargazers_count":([[:digit:]]+).*/\1/'
+
+fetch_stars()
+{
+	num_stars=$(curl -s -A "$user_agent" -H "$header" -H "$accept" "$1" |
+	sed -E 's/.*stargazers_count":([[:digit:]]+).*/\1/')
+	printf '%s\n' "$num_stars"
+}
+
+while read -r line; do
+	# grep 'nicksp' <<< "$line" &> /dev/null && stars=$(fetch_stars "$line")
+	stars=$(fetch_stars "$line")
+	printf '%s\t%s\n' "$line" "${stars-placeholder}";
+done <  <(sed -e 's/https:../&api./g; s/github\.com\//&repos\//g; s/\/$//g' $tmpfile)
